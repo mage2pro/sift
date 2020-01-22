1a8eb0700284a87f3b8b02066cd4914f0dbea8af
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-23 03:57:15 +0600

    no message

---
+++ Js.php
@@ -0,0 +1,6 @@
+<?php
+namespace Dfe\Sift;
+use Magento\Framework\View\Element\Template as _P;
+// 2020-01-23
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Js extends _P {}
