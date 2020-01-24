6e7b2f3fc84cc56ae9ea6d46fd5750bcef870b23
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-25 05:12:57 +0600

    no message

---
+++ T/CaseT.php
@@ -0,0 +1,4 @@
+<?php
+namespace Dfe\Sift\T;
+// 2020-01-25
+abstract class CaseT extends \Df\Core\TestCase {}
---
+++ T/CaseT/SDK.php
@@ -0,0 +1,28 @@
+<?php
+namespace Dfe\Sift\T\CaseT;
+use Dfe\Sift\Settings as S;
+use SiftClient as C;
+// 2020-01-20 https://github.com/SiftScience/sift-php
+final class SDK extends \Dfe\Sift\T\CaseT {
+	/** 2020-01-20 */
+	function t00() {echo __METHOD__;}
+
+	/** @test 2020-01-20 */
+	function t01() {
+		$s = S::s(); /** @var S $s */
+		$c = new C(['account_id' => $s->merchantID(), 'api_key' => $s->backendKey()]); /** @var C $c */
+		$r = $c->track('$transaction', [
+			'$amount' => 15230000,
+			'$currency_code' => 'USD',
+			'$seller_user_id' => '2371',
+			'$time' => 1327604222,
+			'$transaction_id' => '573050',
+			'$user_email' => 'buyer@gmail.com',
+			'$user_id' => '23056',
+			'distance_traveled' => 5.26,
+			'seller_user_email' => 'seller@gmail.com',
+			'trip_time' => 930
+		]);
+		echo df_json_encode($r);
+	}
+}
