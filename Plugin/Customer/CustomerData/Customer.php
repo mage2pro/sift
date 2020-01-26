ce39e6e05c6973f38d1d15cff4f3eecaed683bbc
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-26 08:16:11 +0600

    0.0.6

---
+++ Plugin/Customer/CustomerData/Customer.php
@@ -0,0 +1,24 @@
+<?php
+namespace Dfe\Sift\Plugin\Customer\CustomerData;
+use Magento\Customer\CustomerData\Customer as Sb;
+use Magento\Customer\Model\Session as Sess;
+// 2020-01-26
+final class Customer {
+	/**
+	 * 2020-01-26
+	 * I do not use @see df_customer_session_id() because it is regenerated in many cases (e.g., on login or signup).
+	 * @see \Magento\Customer\CustomerData\Customer::getSectionData()
+	 * @param Sb $sb
+	 * @param array(string => mixed) $r
+	 * @return array(string => mixed)
+	 */
+	function afterGetSectionData(Sb $sb, array $r) {
+		$k = 'siftSessionId'; /** @var string $k */
+		$m = ucfirst($k); /** @var string $m */
+		$sess = df_customer_session(); /** @var Sess $sess */
+		if (!($v = $sess->__call("get$m", []))) { /** @var string $v */
+			$sess->__call("set$m", [$v = df_uid()]);
+		}
+		return [$k => $v] + $r;
+	}
+}
