add8c0602a8405f98b15013a3b69f5bbdcab3c3f
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-03 15:24:42 +0600

    0.1.1

--- Payload/Payment/PayPal.php
+++ Payload/Payment/PayPal.php
@@ -10,10 +10,41 @@ final class PayPal {
 	 * @param P $p
 	 * @return array(string => mixed)
 	 */
-	static function p(P $p) {
-		return [
-			// 2020-02-03 String. «Payer email address returned by Paypal.»
-			'paypal_payer_id' => dfp_iia($p, 'paypal_payer_id')
-		];
-	}
+	static function p(P $p) {return array_map('df_nts', dfp_iia($p,
+		/**
+		 * 2020-02-03 String. «Payer address status returned by Paypal.»
+		 * @see \Magento\Paypal\Model\Info::PAYPAL_ADDRESS_STATUS
+		 */
+		'paypal_address_status'
+		/**
+		 * 2020-02-03 String. «Payer email address returned by Paypal.»
+		 * @see \Magento\Paypal\Model\Info::PAYPAL_PAYER_EMAIL
+		 * An example: «test-buyer@dmitry-fedyuk.com».
+		 */
+		,'paypal_payer_email'
+		/**
+		 * 2020-02-03 String. «Payer ID returned by Paypal.»
+		 * An example: «UES9EX5HHA8ZJ».
+		 * @see \Magento\Paypal\Model\Info::PAYPAL_PAYER_ID
+		 */
+		,'paypal_payer_id'
+		/**
+		 * 2020-02-03 String. «Payer status returned by Paypal.»
+		 * An example: «verified».
+		 * @see \Magento\Paypal\Model\Info::PAYPAL_PAYER_STATUS
+		 */
+		,'paypal_payer_status'
+		/**
+		 * 2020-02-03 String. «Payment status returned by Paypal.»
+		 * An example: «completed».
+		 * @see \Magento\Paypal\Model\Info::PAYMENT_STATUS_GLOBAL
+		 */
+		,'paypal_payment_status'
+		/**
+		 * 2020-02-03 String. «Seller protection eligibility returned by Paypal.»
+		 * An example: «Ineligible».
+		 * @see \Magento\Paypal\Model\Info::PAYPAL_PROTECTION_ELIGIBILITY
+		 */
+		,'paypal_protection_eligibility'
+	)) /** @uses df_nts() */;}
 }
--- Test/CaseT/PayPal.php
+++ Test/CaseT/PayPal.php
@@ -11,6 +11,8 @@ final class PayPal extends \Dfe\Sift\Test\CaseT {
 	function t01() {
 		$p = dfp(10); /** @var P $p */
 		print_r(dfp_iia($p));
-		print_r(dfp_iia($p, 'paypal_payer_id'));
+		echo df_json_encode(array_map('df_nts', dfp_iia($p,
+			'paypal_payer_id', 'paypal_payer_email', 'paypal_payer_status', 'ttt'
+		))); /** @uses df_nts() */
 	}
 }
