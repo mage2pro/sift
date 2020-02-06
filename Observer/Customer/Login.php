296c78fa3b16cf05f0d601dc7835c0f5b1f70d58
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-06 19:38:42 +0600

    0.2.1

--- Observer/Customer/Login.php
+++ Observer/Customer/Login.php
@@ -30,10 +30,6 @@ final class Login implements ObserverInterface {
 		$c = $o['customer']; /** @var C $c */
 		// 2020-02-06 https://sift.com/developers/docs/curl/events-api/reserved-events/login
 		Event::p('login', pLoginOrRegister::p($c) + [
-			// 2020-02-06
-			// «Use `login_status` to represent the success or failure of the login attempt»
-			// Allowed values: `$success`, `$failure`.
-			'login_status' => sift_prefix('success')
 			/**
 			 * 2020-02-06
 			 * «Capture the reason for the failure of the login.
@@ -44,7 +40,11 @@ final class Login implements ObserverInterface {
 			 * 		$wrong_password: Username exists, but the password is incorrect for this user.
 			 * »
 			 */
-			,'failure_reason' => ''
+			'failure_reason' => ''
+			// 2020-02-06
+			// «Use `login_status` to represent the success or failure of the login attempt»
+			// Allowed values: `$success`, `$failure`.
+			,'login_status' => sift_prefix('success')
 			,'username' => $c->getEmail() // 2020-02-06 String. «The username entered at the login prompt»
 		]);
 	});}
