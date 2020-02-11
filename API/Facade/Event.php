efe5e0fea880eda06d52d482d838e84297f26e05
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-11 23:56:04 +0600

    https://github.com/dmitry-fedyuk/sift/issues/11

--- API/Facade/Event.php
+++ API/Facade/Event.php
@@ -4,4 +4,19 @@ namespace Dfe\Sift\API\Facade;
  * 2020-01-25
  * @method static Event s()
  */
-final class Event extends \Df\API\Facade {}
+final class Event extends \Df\API\Facade {
+	/**
+	 * 2020-02-11
+	 * 1) «To get scores back when sending an event,
+	 * just append the query parameter return_score=true to your API request.»
+	 * https://sift.com/developers/docs/php/score-api/overview
+	 * 2) "Integrate the module with the Score API": https://github.com/dmitry-fedyuk/sift/issues/11
+	 * @override
+	 * @see \Df\API\Facade::path()
+	 * @used-by \Df\API\Facade::p()
+	 * @param int|string|null $id
+	 * @param string|null $suffix
+	 * @return string
+	 */
+	protected function path($id, $suffix) {return parent::path($id, $suffix) . '?return_score=true';}
+}
