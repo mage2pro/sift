6aa2d933be2b1b845d3fd111641e5612db84f7c0
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 07:33:55 +0000

    https://github.com/mage2pro/core/issues/168

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -23,9 +23,8 @@ class Index extends \Df\Framework\Action implements IPost {
 	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
 	 * 		$result = $this->execute();
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
-	 * @return Text
 	 */
-	function execute() {
+	function execute():Text {
 		/** @var Text $r */
 		try {
 			$this->checkSignature();
