74c3fa4a87732a17deb37d613df85756406cf561
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 07:48:23 +0000

    https://github.com/mage2pro/core/issues/174

--- Plugin/Customer/Model/AccountManagement.php
+++ Plugin/Customer/Model/AccountManagement.php
@@ -17,7 +17,6 @@ final class AccountManagement {
 	 * Actally, the method still exists in the latest Magento version (2.3.4),
 	 * but it is deprecated and is not used:
 	 * https://github.com/magento/magento2/blob/2.3.4/app/code/Magento/Customer/Model/AccountManagement.php#L1493-L1520
-	 * @param Sb $sb
 	 * @param ICD|CD $cd
 	 */
 	function beforePasswordResetConfirmation(Sb $sb, ICD $cd) {B::reset($cd);}
